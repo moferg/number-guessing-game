e9e596592f543cdc7db899a72f79636af8a6519b
Marshall Ferguson <moferg01@louisville.edu>
2020-06-27 09:39:37 -0400

    added TODOs

---
+++ number_guessing_game.py
@@ -0,0 +1,10 @@
+# Number Guessing Game - Marshall Ferguson - 6/2020
+
+# TODO - Generate a random number between 1 and a user-defined upper limit
+
+# TODO - Prompt the user to guess the random number
+    # TODO - Let the user know if the number they guessed was higher or lower than generated number
+
+# TODO - Increment the user's guess count if they guess incorrectly
+
+# TODO - Print out the generated number and guess count when user guesses correctly
